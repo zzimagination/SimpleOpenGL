b3ec85c7015b47c93444ad85b883faf721b6219b
zzimagination <zhaoimag@163.com>
2021-02-21 18:18:35 +0800

    增加resource config。

---
+++ Engine/SemperEngine/Program/ResourceConfig.h
@@ -0,0 +1,19 @@
+#ifndef __RESOURCE_CONFIG__
+#define __RESOURCE_CONFIG__
+
+namespace SemperEngine
+{
+	class ResourceConfig
+	{
+	public:
+
+		enum class TextureFilter
+		{
+			Nearest,
+
+			Linear
+		};
+	};
+}
+
+#endif // !__RESOURCE_CONFIG__
